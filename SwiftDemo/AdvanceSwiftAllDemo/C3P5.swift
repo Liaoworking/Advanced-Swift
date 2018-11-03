42bc0ad845dafd5663bb3a74a527953bd243265e
liaoworking <liaoworking@gmail.com>
2018-11-03 17:25:21 +0800

    Chapter 3 done~

---
+++ SwiftDemo/AdvanceSwiftAllDemo/C3P5.swift
@@ -0,0 +1,29 @@
+//
+//  C3P5.swift
+//  AdvanceSwiftAllDemo
+//
+//  Created by liaoworking on 2018/11/3.
+//  Copyright © 2018 liaoworking. All rights reserved.
+//
+
+import UIKit
+///专门的集合类型
+//class C3P5: Collection {
+//
+//}
+
+//extension C3P5: BidirectionalCollection{
+//
+//}
+//
+//extension C3P5: RandomAccessCollection{
+//
+//}
+//
+//extension C3P5: MutableCollection{
+//
+//}
+//
+//extension C3P5: RangeReplaceableCollection{
+//
+//}
