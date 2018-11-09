542d296a3772daea04afd48b0d5a90d0312c89d1
liaoworking <liaoworking@gmail.com>
2018-11-10 01:03:53 +0800

    C4P4 Overview Of Optional Done.  今天的内容有点意思 大家周末快乐~

--- SwiftDemo/AdvanceSwiftAllDemo/C4P3.swift
+++ SwiftDemo/AdvanceSwiftAllDemo/C4P3.swift
@@ -39,7 +39,73 @@ class C4P3: NSObject {
         var number: Int?
         number = nil
         String(number ?? 5)
+        
+        
+        
+        ///flatMap在可选值中的用法，感觉这个用法很有用啊
+        let urlString = "http://www.objc.io/logo.png"
+        let view = URL(string: urlString)
+            .flatMap { (url) -> Data? in
+            try? Data(contentsOf: url)
+            }
+            .flatMap { (data) -> UIImage? in
+                UIImage(data: data)
+            }
+            .map { (image) -> UIImageView in
+                UIImageView(image: image)
+        }
+        
+        
+        let view2 = URL(string: urlString)
+            .flatMap {
+                try? Data(contentsOf: $0)
+            }
+            .flatMap {
+                UIImage(data: $0)
+            }
+            .map {
+                UIImageView(image: $0)
+        }
+        
+        if let view2 = view2 {
+            UIView().addSubview(view2)
+        }
+        
+        
+        
+        
+        ///flatMap 过滤nil
+        let numbers = ["1","2","3","4","liaoworking"]
+        
+        var sum = 0
+        for case let i? in numbers.map({
+            Int($0)
+        }) {
+            sum += i// Int($0)为nil就不走这里了
+        }
+        //        sum的值为10
+        
+        ///当我们用?? 把nil替换成0
+        numbers.map { Int($0) }.reduce(0) { $0 + ($1 ?? 0)} //10
+        
+        ///在标准库中flatMap的作用可能正是你想要
+        numbers.flatMap { Int($0) }.reduce(0, +) // 10
+        
+
+        
+        /// switch case
+        for i in numbers {
+            switch Int(i) {
+            case 0:
+                print("0")
+            case nil:
+                print("can't convert to int")
+            default:
+                print("一个大数")
+            }
+        }
     }
+
     
 }
 
